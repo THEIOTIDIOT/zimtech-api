990b2546f63826b221dcc1bcfa2cad441b3d270d
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-01-18 22:22:27 -0600

    initial commit

---
+++ project/server/config.py
@@ -0,0 +1,51 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+import configparser
+
+# Config variables
+parser = configparser.ConfigParser()
+parser.read("config.ini")
+username = parser["postgres"]["username"]
+password = parser["postgres"]["password"]
+server_host = parser["postgres"]["server_host"]
+db_name = parser["postgres"]["db_name"]
+secret_key = parser["flask"]["secret_key"]
+postgres_connection = f"postgresql://{username}:{password}@{server_host}/{db_name}"
+
+
+class BaseConfig:
+    """Base configuration."""
+
+    SECRET_KEY = secret_key
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+
+    SECRET_KEY = secret_key
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_connection
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration."""
+
+    SECRET_KEY = secret_key
+    DEBUG = True
+    TESTING = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_connection + "_test"
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration."""
+
+    SECRET_KEY = "my_precious"
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = postgres_connection
