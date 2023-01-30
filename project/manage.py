52d4a509e44696d409d6ad18d967cfa82b60bc2b
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-01-29 22:38:44 -0600

    updating commands

--- project/manage.py
+++ project/manage.py
@@ -8,8 +8,7 @@ export FLASK_APP=app.py
 flask --app project.server db init
 flask --app project.server db migrate
 flask --app project.server db upgrade
-flask --app project.server 
-run
+flask --app project.server --debug run
 
 """
 # TODO: Create a management utility for dealing with testing and database initialization/migration/upgrade routines
