f9bf0d44bf9a2814d866ba17e23f4771020b8a5c
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-25 15:56:42 -0500

    deployment updates

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,6 @@
-sudo docker run --rm --name zimtechapi \
+#!/bin/bash
+sudo docker run --rm --name api \
     --network zimnet \
-    --ip 172.27.0.4 \
-    -d --restart=unless-stopped -p 5000:5000 zimtechapi:latest
+    -d --restart=unless-stopped -p 5000:5000 zimtechapi:latest \
+    && sudo docker exec -it swag rm /config/nginx/proxy-confs/api.subdomain.conf \
+    && sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs
