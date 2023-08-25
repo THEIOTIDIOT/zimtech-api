8bba06d714b05400196dfe90d4c0b5172e525e6b
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-25 16:39:23 -0500

    conflicting commands

--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo docker run --rm --name api \
     --network zimnet \
-    -d --restart=unless-stopped -p 5000:5000 zimtechapi:latest \
+    -d -p 5000:5000 zimtechapi:latest \
     && sudo docker exec -it swag rm /config/nginx/proxy-confs/api.subdomain.conf \
     && sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs
