77150f02b91ca1b7358e0939f5bdc2b90851a23b
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-25 21:25:18 -0500

    restart policy

--- run_container.sh
+++ run_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker run --rm --name api \
+sudo docker run --name api \
     --network zimnet \
-    -d -p 5000:5000 zimtechapi:latest \
+    -d --restart=unless-stopped --p 5000:5000 zimtechapi:latest \
     && sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs
