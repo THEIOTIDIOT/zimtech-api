f02a5ebf9de5e82df0070ebd88674f8c1a58585f
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-25 21:22:48 -0500

    copy

--- run_container.sh
+++ run_container.sh
@@ -2,5 +2,4 @@
 sudo docker run --rm --name api \
     --network zimnet \
     -d -p 5000:5000 zimtechapi:latest \
-    && sudo docker exec -it swag rm /config/nginx/proxy-confs/api.subdomain.conf \
     && sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs
