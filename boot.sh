82fbd75a6b473ca6aaf96ecd7360dca2c6068a17
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-25 23:30:39 -0500

    cors

--- boot.sh
+++ boot.sh
@@ -4,5 +4,5 @@ flask --app "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['api.b
 exec gunicorn -b :5000 -w 4 \
     --access-logfile=- \
     --error-logfile=- \
-    #"zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['https://*.benzimmer.us', 'https://zimmertechnologies.com'])"
-    "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['*'])"
+    "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['https://www.benzimmer.us', 'https://www.zimmertechnologies.com'])"
+    # "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['*'])"
