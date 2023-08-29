9c31490d7a237e16223d2db2d44a07f52064c1db
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-28 22:30:22 -0500

    reconfiguring deployment scripts

--- boot.sh
+++ boot.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-flask --app "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['api.benzimmer.us', 'blog.benzimmer.us'])" db upgrade
-exec gunicorn -b :5000 -w 4 --access-logfile=- --error-logfile=- "zimtechapi:create_app('zimtechapi.config.ProductionConfig')"
-    # "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['https://www.benzimmer.us', 'https://www.zimmertechnologies.com'])"
-    # "zimtechapi:create_app('zimtechapi.config.ProductionConfig', [r'https://*.benzimmer.us/*'])"
+flask --app "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['https://api.benzimmer.us', 'https://www.benzimmer.us', 'https://benzimmer.us'])" db upgrade
+exec gunicorn -b :5000 -w 4 --access-logfile=- --error-logfile=- "zimtechapi:create_app('zimtechapi.config.ProductionConfig', ['https://api.benzimmer.us', 'https://www.benzimmer.us', 'https://benzimmer.us'])"
--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo docker run --name api \
     --network zimnet \
-    -d --restart=unless-stopped -p 5000:5000 zimtechapi:latest \
-    && sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs \
-    && sudo docker restart swag
+    -d --restart=unless-stopped -p 5000:5000 zimtechapi:latest
+
+sudo docker cp $HOME/Git/zimtech-api/api.subdomain.conf swag:/config/nginx/proxy-confs
+
+sudo docker restart swag
